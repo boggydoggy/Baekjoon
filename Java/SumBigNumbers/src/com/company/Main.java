4039bc48fb8fb988221cfc0763e58c240b500169
boggydoggy <zhd401@naver.com>
2021-12-01 16:11:11 +0900

    chore: SumBigNumbers Problem succeed

--- Java/SumBigNumbers/src/com/company/Main.java
+++ Java/SumBigNumbers/src/com/company/Main.java
@@ -9,7 +9,8 @@ public class Main {
 
         String a = sc.next(), b = sc.next();
 
-        int[] parseA = new int[a.length() + 1], parseB = new int[b.length() + 1];
+        int maxLength = Math.max(a.length(), b.length());
+        int[] parseA = new int[maxLength + 1], parseB = new int[maxLength + 1];
 
         for(int i = 0, j = a.length() - 1; i < a.length(); i++, j--) {
             parseA[i] = a.charAt(j) - '0';
@@ -18,43 +19,20 @@ public class Main {
             parseB[i] = b.charAt(j) - '0';
         }
 
-        int min_length;
-        int[] ans;
-        int[] less;
-
-
-        if(a.length() > b.length()) {
-            min_length = b.length();
-            ans = parseA.clone();
-            less = parseB.clone();
-        }
-        else {
-            min_length = a.length();
-            ans = parseB.clone();
-            less = parseA.clone();
+        for(int i = 0; i < maxLength; i++) {
+            int value = parseA[i] + parseB[i];
+            parseA[i] = value % 10;
+            parseA[i+1] += value / 10;
         }
 
-        for(int i = 0; i < min_length; i++) {
-            if(ans[i] + less[i] > 9) {
-                ans[i] = (ans[i] + less[i]) % 10;
-                ans[i+1] += 1;
-            }
-            else {
-                ans[i] += less[i];
-            }
-
-        }
-
-        int ansLength = ans.length - 1;
-
-        if(ans[ans.length - 1] == 0) {
-            ansLength -= 1;
+        if(parseA[maxLength] == 0) {
+            maxLength -= 1;
         }
 
         StringBuilder sb = new StringBuilder();
 
-        for(int i = ansLength; i >= 0; i--) {
-            sb.append(ans[i]);
+        for(int i = maxLength; i >= 0; i--) {
+            sb.append(parseA[i]);
         }
 
         System.out.println(sb);
