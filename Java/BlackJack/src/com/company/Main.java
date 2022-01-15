13d4241c21ea7dd3e39d2888f569760a15d686f8
boggydoggy <zhd401@naver.com>
2022-01-15 18:22:35 +0900

    chore: change BlackJack Problem, succeed

--- Java/BlackJack/src/com/company/Main.java
+++ Java/BlackJack/src/com/company/Main.java
@@ -49,14 +49,17 @@ public class Main {
     }
 
     public static int blackJack(int n, int m) {
-        int sum, ans = 0, check = m;
+        int sum, ans = 0;
 
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
+        for(int i = 0; i < n - 2; i++) {
+            for(int j = i + 1; j < n - 1; j++) {
                 for(int k = j + 1; k < n; k++) {
                     sum = arr[i] + arr[j] + arr[k];
-                    if((m - sum) < check && (m - sum) > 0) {
-                        check = m - sum;
+
+                    if(sum == m) {
+                        return sum;
+                    }
+                    if(sum < m && sum > ans) {
                         ans = sum;
                     }
                 }
