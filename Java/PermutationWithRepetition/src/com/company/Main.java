63f74636072404b257bfd35a01b07565bd6cc7ef
boggydoggy <zhd401@naver.com>
2022-03-28 17:09:58 +0900

    chore: add Permutation With Repetition Problem

---
+++ Java/PermutationWithRepetition/src/com/company/Main.java
@@ -0,0 +1,58 @@
+/*
+문제
+자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+
+1부터 N까지 자연수 중에서 M개를 고른 수열
+같은 수를 여러 번 골라도 된다.
+입력
+첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 7)
+
+출력
+한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+
+수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+
+sol: 중복 순열, Permutation Problem에서 중복 조건 제거
+ */
+
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int N;
+    public static int M;
+    public static int[] arr;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+
+        dfs(0);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int depth) {
+        if(depth == M) {
+            for(int i: arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0; i < N; i++) {
+            arr[depth] = i + 1;
+            dfs(depth + 1);
+        }
+    }
+}
